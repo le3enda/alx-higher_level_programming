76fdb131b115dd9369927ee36cd3db1a655f5f1a
le3enda <le3enda@gmail.com>
2023-12-05 14:25:08 -0800

    o

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -8,9 +8,11 @@
  */
 void print_python_list_info(PyObject *p)
 {
-int elem;
+	int elem;
+
 	printf("[*] Size of the Python List = %lu\n", Py_SIZE(p));
 	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
 	for (elem = 0; elem < Py_SIZE(p); elem++)
 		printf("Element %d: %s\n", elem, Py_TYPE(PyList_GetItem(p, elem))->tp_name);
+}
 }
