d840a4ec6a320eabbdc046cad7b1aae2c00cfd0e
le3enda <le3enda@gmail.com>
2023-12-18 16:40:39 -0800

    op

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    num = 0
-    for i in range(x):
+    i = 0
+    while i is not x:
         try:
             print(my_list[i], end="")
-            num += 1
+            i += 1
         except IndexError:
-            break
-    print("")
+            None
+    print()
     Return 1
